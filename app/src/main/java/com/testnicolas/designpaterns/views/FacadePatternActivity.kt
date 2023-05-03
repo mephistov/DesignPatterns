e141e427b2d7add733794473a84fd549e0589fb3
GLOBANT\nicolas.castilla <nicolas.castilla@globant.com>
2023-05-03 15:57:40 -0500

    dependency inyection

--- app/src/main/java/com/testnicolas/designpaterns/views/FacadePatternActivity.kt
+++ app/src/main/java/com/testnicolas/designpaterns/views/FacadePatternActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import com.bumptech.glide.RequestManager
 import com.testnicolas.designpaterns.R
 import com.testnicolas.designpaterns.databinding.ActivityFacadePatternBinding
 import com.testnicolas.designpaterns.databinding.ActivityFactoryPatternBinding
 import com.testnicolas.designpaterns.viewmodels.FacadePatterViewModel
 import com.testnicolas.designpaterns.viewmodels.FactoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FacadePatternActivity : AppCompatActivity() {
 
     private val viewModel: FacadePatterViewModel by viewModels()
     private lateinit var binding: ActivityFacadePatternBinding
+    @Inject lateinit var glide: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +36,7 @@ class FacadePatternActivity : AppCompatActivity() {
         binding.button4.setOnClickListener {
             viewModel.orderFood("1")
         }
+
+        glide.load("https://media-cdn.tripadvisor.com/media/photo-s/19/a4/6c/82/dining-and-bar-area.jpg").into(binding.imageView2)
     }
 }
